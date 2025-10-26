a856439a73599ac0fb72c2e6834adef7369b6c19
himerik <himirik@gmail.com>
2025-10-26 11:07:46 +0400

    Add files via upload

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1761407580,
+	"version": 1761462347,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -31,7 +31,9 @@
 		"images/cracks-sheet0.png",
 		"images/sprite4-sheet0.png",
 		"images/startbutton-sheet0.png",
+		"images/player2-sheet0.png",
 		"images/bulletui-sheet0.png",
+		"images/flash-sheet0.png",
 		"media/alesis-s4-plus-harpsifunk-c3.m4a",
 		"media/alesis-s4-plus-harpsifunk-c3.ogg",
 		"media/alesis-s4-plus-harpsifunk-c4.m4a",
